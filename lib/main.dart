7ba9509c3c2c2da6234472fb2b2645f825c2dc34
gammastani <46166520+gammastani@users.noreply.github.com>
2023-11-26 22:04:14 +0100

    firebase auth not working version

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:yellafirstversion/pages/auth_page.dart';
 import 'package:yellafirstversion/pages/login_page.dart';
 
-Future<void> main() async {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // try {
-  //   await Firebase.initializeApp();
-  //   print('Firebase initialized successfully');
+    await Firebase.initializeApp();
     runApp(MaterialApp(
         home: LoginPage()));
-  // } catch (e) {
-  //   print('Error initializing Firebase: $e');
-  // }
 }
 
 class MyApp extends StatelessWidget {
